16b4444cb437f75e7c4084caa9c9de656d8b4e27
SooBo <sjstnqh@naver.com>
2025-02-26 23:42:53 +0900

    feat: 다운로드 버튼에 깃허브 릴리지 파일 다운로드 링크 연결

--- src/components/Container/ContentContainer.jsx
+++ src/components/Container/ContentContainer.jsx
@@ -1,13 +1,22 @@
 function ContentContainer() {
   return (
     <div className="w-[50%] h-full flex justify-center items-center z-20 overflow-y-scroll">
-      <div className="w-[70%] h-[50%] p-18 bg-subsub text-center rounded-xl shadow-lg shadow-blue-500/50">
+      <div className="relative w-[70%] h-[52%] p-18 bg-subsub text-center rounded-xl shadow-lg shadow-blue-500/50">
         <div className="text-7xl mb-10 text-shadow">Auto Page</div>
-        <h1 className="mb-10 text-3xl">Download App</h1>
-        <p className="text-xl">Click the button! start your download.</p>
-        <button className="w-40 h-15 m-10 bg-blue text-xl rounded-xl hover:bg-[#21a1f1] hover:scale-125 active:scale-90 transition duration-300 animate-bounce cursor-pointer">
-          Download
-        </button>
+        <h1 className="text-3xl">Download App</h1>
+        <p className="my-10 text-xl">Click the button! start your download.</p>
+        <div className="flex justify-center items-center">
+          <a href="https://github.com/soobolee/auto-page/releases/download/v1.0.0-mac/AutoPage-darwin-arm64.zip">
+            <button className="p-2 mx-3 bg-blue text-xl rounded-xl hover:bg-[#21a1f1] hover:scale-125 active:scale-90 transition duration-300 animate-bounce cursor-pointer">
+              Download for Mac
+            </button>
+          </a>
+          <a href="https://github.com/soobolee/auto-page/releases/download/v1.0.0-window/AutoPage-win32-x64.zip">
+            <button className="p-2 mx-3 bg-blue text-xl rounded-xl hover:bg-[#21a1f1] hover:scale-125 active:scale-90 transition duration-300 animate-bounce cursor-pointer">
+              Download for Window
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
